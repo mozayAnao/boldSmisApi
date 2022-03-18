5ece504216aeb3d1a0f420bee4612ccb9ee13364
mozay <mosesanao13@gmail.com>
2022-03-18 15:49:36 +0000

    server started

--- app.js
+++ app.js
@@ -38,4 +38,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(process.env.PORT || '3000', function() {
+  console.log(`App started`)
+});
